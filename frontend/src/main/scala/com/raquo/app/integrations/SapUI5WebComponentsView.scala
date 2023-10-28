55c088ceba92e39bbfc1556e93dc7f36f0958d2d
Nikita Gazarov <nikita@raquo.com>
2023-10-27 22:11:01 -0700

    UI5 components custom styling

--- frontend/src/main/scala/com/raquo/app/integrations/SapUI5WebComponentsView.scala
+++ frontend/src/main/scala/com/raquo/app/integrations/SapUI5WebComponentsView.scala
@@ -45,7 +45,43 @@ object SapUI5WebComponentsView {
         _.level := TitleLevel.H2,
         _ => "Multi token input"
       ),
-      renderMultiTokenInput()
+      renderMultiTokenInput(),
+
+      Title.of(
+        _.level := TitleLevel.H2,
+        _ => "Buttons"
+      ),
+      div(renderButtons()),
+
+      br(),
+
+      p("All of the above are SAP UI5 components – titles, date picker, combo boxes, buttons. Some of them are custom-styled using different methods. See SapUI5WebComponentsView.scala and SapUI5WebComponentsView.less for details."),
+      p(a("SAP UI5", href("https://sap.github.io/ui5-webcomponents/playground/")), " is a pretty decent library of web components. We use it via ", a("LaminarSAPUI5Bindings", href("https://github.com/sherpal/LaminarSAPUI5Bindings")), " – good, manually crafted UI5 bindings designed specifically for Laminar. ", a("See live demo", href("https://sherpal.github.io/laminar-ui5-demo/"))),
+      p("You can use other web components by creating bindings for them. It's not too much effort – the pattern is very simple, if you have documentation listing the methods and the properties of your components, it's trivial. You can do it incrementally too, define components and their properties as you start using them.")
+    )
+  }
+
+  private def renderButtons(): List[Modifier.Base] = {
+    List(
+      Button.of(
+        _ => "CLICK ME",
+        _ => onClick --> { _ => dom.window.alert("YOUR COMPLIANCE IS APPRECIATED") }
+      ),
+      " ",
+      Button.of(
+        _ => cls("x-yellow"),
+        _ => "Button styled via CSS", // See SapUI5WebComponentsView.less
+        _ => onClick --> { _ => dom.window.alert("Ok") }
+      ),
+      " ",
+      Button.of(
+        _ => backgroundColor := "green",
+        _ => borderColor := "green",
+        _ => color.white,
+        _ => fontWeight.bold,
+        _ => "Button styled via Laminar",
+        _ => onClick --> { _ => dom.window.alert("Ok") }
+      )
     )
   }
 
@@ -106,7 +142,7 @@ object SapUI5WebComponentsView {
       _.slots.tokens <-- tokenValuesVar.signal.map(_.map(tokenValue => MultiInput.token(_.text := tokenValue))),
       _.events.onChange.map(_.target.value) --> changeBus.writer,
       _ => newValuesChanges --> tokenValuesVar.writer,
-      _ => value <-- changeBus.events.mapTo(""), // clear input text when selecting a value
+      _ => value <-- changeBus.events.delay(0).mapTo(""), // clear input text when selecting a value
       _.events.onTokenDelete.map(_.detail.token.text) --> tokenValuesVar.updater((values, toRemove) =>
         values.filterNot(_ == toRemove)
       )
