1b2ef7738b579a13f038c1cf309d50e6183267db
Nikita Gazarov <nikita@raquo.com>
2023-11-01 02:49:09 -0700

    Fix project rename

--- client/dist/assets/ShoelaceWebComponentsView-e9220a04.js
+++
@@ -1,2 +0,0 @@
-const t="";export{t as default};
-//# sourceMappingURL=ShoelaceWebComponentsView-e9220a04.js.map
--- client/dist/assets/icon-bcba9cfe.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./chunk.2HU27ZWH-5a69d77d.js";var e=a;a.define("sl-icon");export{e as default};
-//# sourceMappingURL=icon-bcba9cfe.js.map
