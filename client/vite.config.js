b310e37f5ad1229cdc3c8bc10ff82fd68afa6f4f
Nikita Gazarov <nikita@raquo.com>
2023-12-05 23:18:45 -0800

    Add target-idea dir to glob resolver exclusions

--- client/vite.config.js
+++ client/vite.config.js
@@ -26,7 +26,8 @@ export default defineConfig(
           cwd: __dirname,
           ignore: [
             'node_modules/**',
-            'target/**'
+            'target/**',
+            'target-idea/**'
           ]
         }),
         importSideEffectPlugin({
