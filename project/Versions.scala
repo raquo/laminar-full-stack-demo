e9b7cde0bc5a7976874a343ec342ad943acaa5c5
Nikita Gazarov <nikita@raquo.com>
2024-11-08 18:40:30 -0800

    Build: Bump UI5 version, use Scala LTS again

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,22 +1,20 @@
 object Versions {
 
-  val Scala_3 = "3.4.1" // #TODO Temporarily for UI5. Downgrade to 3.3.1 LTS later.
+  val Scala_3 = "3.3.4"
 
   // -- Shared --
-
-  val JsoniterScala = "2.24.0"
 
   val Borer = "1.14.0"
 
   // -- Frontend --
 
-  val Laminar = "17.0.0"
+  val Laminar = "17.1.0"
 
   val LaminarShoelace = "0.1.0"
 
   val Waypoint = "8.0.0"
 
-  val UI5 = "1.21.1" // #TODO Bump when version released for Laminar v17 final. This one is for 17.0.0-M8 but it should work fine.
+  val UI5 = "2.0.0"
 
   // -- Backend --
 
